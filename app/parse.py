d98dd1774c23d81dc3aee4524a8c207700b06854
ratatuiii <orest.chupa@gmail.com>
2026-07-13 23:55:59 +0300

    Add parse.py logic

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, astuple, fields
+import csv
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
 
 
 @dataclass
@@ -8,8 +14,53 @@ class Quote:
     tags: list[str]
 
 
+BASE_URL = "https://quotes.toscrape.com/"
+
+
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    return Quote(
+        text=quote_soup.select_one(".text").text,
+        author=quote_soup.select_one(".author").text,
+        tags=[tag.text for tag in quote_soup.select(".tags .tag")],
+    )
+
+
+def get_quotes_from_page(page_soup: BeautifulSoup) -> list[Quote]:
+    quotes = page_soup.select(".quote")
+    return [parse_single_quote(quote) for quote in quotes]
+
+
+def get_all_quotes() -> list[Quote]:
+    all_quotes = []
+    page_url = BASE_URL
+
+    with requests.Session() as session:
+        while page_url:
+            response = session.get(page_url)
+            response.raise_for_status()
+            page_soup = BeautifulSoup(response.content, "html.parser")
+
+            all_quotes.extend(get_quotes_from_page(page_soup))
+
+            next_button = page_soup.select_one(".next > a")
+            page_url = BASE_URL + next_button["href"] if next_button else None
+
+            # be gentle to the website
+            time.sleep(0.5)
+
+    return all_quotes
+
+
+def write_quotes_to_csv(quotes: list[Quote], output_csv_path: str) -> None:
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow([field.name for field in fields(Quote)])
+        writer.writerows([astuple(quote) for quote in quotes])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_all_quotes()
+    write_quotes_to_csv(quotes, output_csv_path)
 
 
 if __name__ == "__main__":
